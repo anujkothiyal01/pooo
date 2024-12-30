c3730d8aa5caa20b35de8027a37a238570ce41dc
Anuj Kothiyal <anujkothiyal01@gmail.com>
2024-12-30 23:22:06 +0530

    Initial commit

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,3 +1,4 @@
+// pages/api/submit.js
 import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -19,15 +20,13 @@ export default async function handler(req, res) {
       const collection = db.collection('submissions');
       await collection.insertOne({ name, email });
       console.log('Data inserted successfully');
-      return res.status(200).json({ message: 'Form submitted successfully' });
+      res.status(200).json({ message: 'Form submitted successfully' });
     } catch (error) {
       console.error('Error during database operation:', error);
-      return res.status(500).json({ error: 'Database error' });
-    } finally {
-      await client.close();
+      res.status(500).json({ error: 'Database error' });
     }
   } else {
-    // Handle other HTTP methods
-    return res.status(405).json({ error: 'Method Not Allowed' });
+    console.error('Invalid request method:', req.method);
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
